c8e0e796a10d37a2d210a0c8881deebb9e57a48b
王大虎 <wangdh3078@126.com>
2017-10-09 11:08:13 +0800

    1

---
+++ MyBlog.Web/Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using MyBlog.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlog.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly BlogService _blogService;
+
+        public SearchController(BlogService blogService)
+        {
+            _blogService = blogService;
+        }
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
