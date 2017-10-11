6ae6bcdde206b318b5f9b65225877ce1f1a717e4
王大虎 <wangdh3078@126.com>
2017-10-11 11:09:42 +0800

    添加API

---
+++ MyBlog.Web/API/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyBlog.Web.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AuthController : ApiController
+    {
+        // GET api/<controller>
+        /// <summary>
+        /// 测试
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        /// <summary>
+        /// 测试2
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- MyBlog.Web/Global.asax.cs
+++ MyBlog.Web/Global.asax.cs
@@ -1,20 +1,20 @@
 ﻿using MyBlog.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using MyBlog.Web.App_Start;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace MyBlog.Web
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
 
             MyBlogEngine.Initialize();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
